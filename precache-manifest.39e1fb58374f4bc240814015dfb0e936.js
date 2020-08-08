903d5806d001da50663995803cd29ed816a9224b
ValentineEssuman <jusvaless@gmail.com>
2020-08-08 23:09:18 +0000

    Updates

---
+++ precache-manifest.39e1fb58374f4bc240814015dfb0e936.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8793f50b860768810211532f94569bc2",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "8b55446a861e7c285eb8",
+    "url": "/me/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d495a21252df1c024707",
+    "url": "/me/static/js/2.af957ace.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/me/static/js/2.af957ace.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b55446a861e7c285eb8",
+    "url": "/me/static/js/main.d2b33236.chunk.js"
+  },
+  {
+    "revision": "2e7f04966b2ab336ece0",
+    "url": "/me/static/js/runtime-main.edc27132.js"
+  }
+]);
