bacf0808ca10e57ef7f12bcc5b2be301562db1d6
ValentineEssuman <jusvaless@gmail.com>
2020-08-09 00:14:19 +0000

    pushing portfolio creation to github

---
+++ build/precache-manifest.55449c1e69aa742f2ae81e809981db85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc81ab685dab79eb266e69e38efead4c",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "48e809ae62ef7eb24f89",
+    "url": "/me/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d495a21252df1c024707",
+    "url": "/me/static/js/2.af957ace.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/me/static/js/2.af957ace.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48e809ae62ef7eb24f89",
+    "url": "/me/static/js/main.e48a2897.chunk.js"
+  },
+  {
+    "revision": "2e7f04966b2ab336ece0",
+    "url": "/me/static/js/runtime-main.edc27132.js"
+  }
+]);
