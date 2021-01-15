68d98c580f5223cc86e03d40197a16e0264f5910
ValentineEssuman <jusvaless@gmail.com>
2021-01-15 15:42:07 +0000

    removed component and links library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import { Route, BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
 // import Home from './components/home'
