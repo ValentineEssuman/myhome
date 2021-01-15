1e55b1ece0ec5b4be3a9509bd85ff4bb512f1ffe
ValentineEssuman <jusvaless@gmail.com>
2021-01-15 16:00:49 +0000

    fixing href line 16

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -6,14 +6,14 @@ export default class Introduction extends Component {
             <section id="colorlib-hero" className="js-fullheight" data-section="home">
 				<div className="flexslider js-fullheight">
 					<ul className="slides">
-				   	<li style={{ backgroundImage: "url(images/newfree.jpg)"}}>
+				   	<li style={{ backgroundImage: "url(.images/newfree.jpg)"}}>
 				   		<div className="overlay"></div>
 				   		<div className="container-fluid">
 				   			<div className="row">
 					   			<div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
 					   				<div className="slider-text-inner js-fullheight">
 					   					<div className="desc">
-												<p><a className="btn btn-primary btn-learn">Checkout My Story <i className="icon-download4"></i></a></p>
+												<p><a className="btn btn-primary btn-learn" href ="#">Checkout My Story <i className="icon-download4"></i></a></p>
 											</div>
 					   				</div>
 					   			</div>
